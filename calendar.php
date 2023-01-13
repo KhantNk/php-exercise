ca2ad67cf0ebf74b7ea517644820e771f43b256b
KhantNyarKo <cgm.khantnyarko@gmail.com>
2023-01-13 16:15:06 +0630

    fixed

--- calendar.php
+++ calendar.php
@@ -2,40 +2,75 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="calendar.css">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="calendar.css">
+  <title>Document</title>
 </head>
 
 <body>
-    <?php
+  <form action="calendar.php" method="post">
+    <label for="month">Month</label>
+    <select name="month" id="month">
+      <option value="">Choose a month</option>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      <option value="7">7</option>
+      <option value="8">8</option>
+      <option value="9">9</option>
+      <option value="10">10</option>
+      <option value="11">11</option>
+      <option value="12">12</option>
+    </select>
+    <input type="submit" value="Submit">
+  </form>
+
+
+  <h1>Calendar for
+    <?php echo Date::MONTH ?>
+    <?php echo Date::YEAR ?> (Myanmar)
+  </h1>
+
+  <?php
+    class Date
+    {
+        const MONTH = 11;
+        const YEAR = 2024;
+    }
+
     $time = time();
     $numDay = date('d', $time);
-    $numMonth = date('m', $time);
+    $numMonth = Date::MONTH;
     $strMonth = date('F', $time);
-    $numYear = date('Y', $time);
+    $numYear = Date::YEAR;
     $firstDay = mktime(0, 0, 0, $numMonth, 1, $numYear);
     $daysInMonth = cal_days_in_month(0, $numMonth, $numYear);
     $dayOfWeek = date('w', $firstDay);
     ?>
-    <table>
-        <caption>Calendar for <?php echo "$strMonth $numYear"; ?></caption>
-        <thead>
-            <tr>
-                <th title="Sunday">S</th>
-                <th title="Monday">M</th>
-                <th title="Tuesday">T</th>
-                <th title="Wednesday">W</th>
-                <th title="Thursday">T</th>
-                <th title="Friday">F</th>
-                <th title="Saturday">S</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <?php
+  <table>
+    <thead>
+      <tr>
+        <?php
+                $mydays = array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');
+
+                foreach ($mydays as $day) {
+                    if (date('D') === 'Sun') {
+                        echo '<td>Mon</td>';
+                    } else {
+                        echo '<td>' . $day . '</td>';
+                    }
+                }
+                ?>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <?php
                 if (0 != $dayOfWeek) {
                     echo ('<td colspan="' . $dayOfWeek . '"> </td>');
                 }
@@ -53,9 +88,9 @@
                     }
                 }
                 ?>
-            </tr>
-        </tbody>
-    </table>
+      </tr>
+    </tbody>
+  </table>
 </body>
 
 </html>
